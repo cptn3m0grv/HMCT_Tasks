69e3624674cbef45bcbc72e51f689a427d7aed9c
Gaurav Goyal <cptn3m0grv@gmail.com>
2020-09-02 22:16:00 +0530

    task 6 done

---
+++ Task6/rds.tf
@@ -0,0 +1,36 @@
+provider "aws" {    
+    access_key = "**"
+    secret_key = "**"
+    token = "**"
+	region = "us-east-1"
+}
+
+resource "aws_db_subnet_group" "default" {
+	    name = "main"
+	    subnet_ids = ["subnet-563f852d","subnet-ae1517c6","subnet-e53645a9"]
+	    tags = {
+	      Name = "my subnets"
+	    }
+	  
+}
+
+resource "aws_db_instance" "sqldb" {
+    engine = "mysql"
+    engine_version = "5.7.30"
+    instance_class = "db.t2.micro"
+    allocated_storage = 10
+    storage_type = "gp2"
+    name = "prodb"
+    username = "sachin"
+    password = "sachin123"
+    port = "3306"
+    publicly_accessible = true
+    skip_final_snapshot = true
+    parameter_group_name = "default.mysql5.7"
+    db_subnet_group_name = "${aws_db_subnet_group.default.name}"
+    tags = {
+      Name = "prodDB"
+    }
+}
+	
+
